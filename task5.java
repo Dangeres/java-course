1917c7417eb69cbb5a11295381fb7e6a75c10ce3
Dangeres <dimon4ik_228@bk.ru>
2018-03-29 16:32:07 +0300

    complete.

--- task5.java
+++ task5.java
@@ -1,1 +1,21 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+
+public class main {
+  
+public static String palindrom(String str){
+		String reverse = new StringBuffer(str).reverse().toString();
+		if(str.equals(reverse)){
+			return "Palindrom!";
+		}
+		
+		return "not palindrom!";
+	}
+
+public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);		
+		String first = sc.nextLine();
+    System.out.println(palindrom(first));
+	}
+}
